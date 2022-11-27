bfe2f73cfc371edc5a315d9e6523fb6061b3c67f
kadun1 <kadunjj@gmail.com>
2022-11-28 01:07:08 +0900

    JPA Modeling Super Class

---
+++ src/main/java/jpabook/chapter7/model5/Child.java
@@ -0,0 +1,19 @@
+package jpabook.chapter7.model5;
+
+import javax.persistence.*;
+
+@Entity
+public class Child {
+
+    @Id
+    private String id;
+
+    @ManyToOne
+    @JoinColumns({
+            @JoinColumn(name = "PARENT_ID1",
+                referencedColumnName = "PARENT_ID1"),
+            @JoinColumn(name = "PARENT_ID2",
+                referencedColumnName = "PARENT_ID2")
+    })
+    private Parent parent;
+}
